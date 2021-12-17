d2ca6e782713aba70c00c8619f5f94e386a0b78e
nikolai <NikolaiMiroshnichenko26@gmail.com>
2021-12-17 14:45:58 +0300

    Add project files.

---
+++ UrhoTestApp/UrhoTestApp/MainPage.xaml.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.ComponentModel;
+using Urho;
+using Xamarin.Forms;
+
+namespace UrhoTestApp
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        private SceneHandler _urhoArea;
+        private float _panCameraPositionX;
+        private float _panCameraPositionY;
+        private float _panCameraPositionZ;
+        private float _panCameraRotationX;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            _urhoArea = await HelloWorldUrhoSurface.Show<SceneHandler>(new Urho.ApplicationOptions(assetsFolder: null));
+        }
+
+        private void X_Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var horizontalPosotion = e.NewValue;
+            if (_urhoArea != null)
+            {
+                var oldYPosition = _urhoArea.CameraNode.Position.Y;
+                var oldZPosition = _urhoArea.CameraNode.Position.Z;
+
+                _urhoArea.CameraNode.Position = new Urho.Vector3((float)horizontalPosotion, oldYPosition, oldZPosition);
+                GetCameraPositionsInfo();
+            }
+        }
+
+        private void Y_Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var verticalPosition = e.NewValue;
+            if (_urhoArea != null)
+            {
+                var oldXPosition = _urhoArea.CameraNode.Position.X;
+                var oldZPosition = _urhoArea.CameraNode.Position.Z;
+
+                _urhoArea.CameraNode.Position = new Urho.Vector3(oldXPosition, (float)verticalPosition, oldZPosition);
+                GetCameraPositionsInfo();
+            }
+        }
+
+        private void Z_Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var zPosition = e.NewValue;
+            if (_urhoArea != null)
+            {
+                var oldXPosition = _urhoArea.CameraNode.Position.X;
+                var oldYPosition = _urhoArea.CameraNode.Position.Y;
+
+                _urhoArea.CameraNode.Position = new Vector3(oldXPosition, oldYPosition, (float)zPosition);
+                GetCameraPositionsInfo();
+            }
+        }
+
+        //cicrle rotation
+        private void X_Rotation_Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var oldXPosition = _urhoArea.CameraNode.Position.X;
+
+            //first quater
+            if (e.NewValue <= 5)
+            {
+                
+                _urhoArea.CameraNode.Rotation = new Quaternion((float)MathHelper.RadiansToDegrees((Math.Acos((5-e.NewValue)/5))),0,0);
+                _urhoArea.CameraNode.Position = new Vector3(oldXPosition, GetYCoordinate(e.NewValue), (float)e.NewValue);
+                var xCircleRotation = _urhoArea.CameraNode.Rotation.X;
+
+                var xAngle = _urhoArea.CameraNode.Rotation.ToEulerAngles();                
+                rotationValueLabel.Text = xAngle.X.ToString();
+
+                sliderValueLabel.Text = sliderRotation.Value.ToString("n2");
+                GetCameraPositionsInfo();
+            }
+
+            //second quater
+            if ((e.NewValue >= 5) && (e.NewValue < 10))
+            {
+                var asd = _urhoArea.CameraNode.Rotation.ToEulerAngles();
+               
+                _urhoArea.CameraNode.Position = new Vector3(oldXPosition, GetYCoordinate(e.NewValue), (float)e.NewValue);
+                _urhoArea.CameraNode.Rotation = new Quaternion((float)(180 -  MathHelper.RadiansToDegrees(Math.Acos((e.NewValue-5)/5))), 0, 0);
+
+                var xCircleRotation = _urhoArea.CameraNode.Rotation.X;
+                var xAngle = _urhoArea.CameraNode.Rotation.ToEulerAngles();
+                rotationValueLabel.Text = xAngle.X.ToString();
+
+                sliderValueLabel.Text = sliderRotation.Value.ToString("n2");
+                GetCameraPositionsInfo();
+            }
+
+            //third quater
+            if ((e.NewValue >= 10) && (e.NewValue < 15))
+            {
+                _urhoArea.CameraNode.Rotation = new Quaternion((float)(270 - MathHelper.RadiansToDegrees((Math.Asin((15-e.NewValue) / 5)))), 0, 0);
+                _urhoArea.CameraNode.Position = new Vector3(oldXPosition, -GetYCoordinate(20 - e.NewValue), (float)(20 - e.NewValue));
+
+                var xCircleRotation = _urhoArea.CameraNode.Rotation.X;
+                var xAngle = _urhoArea.CameraNode.Rotation.ToEulerAngles();
+                rotationValueLabel.Text = xAngle.X.ToString();
+
+                sliderValueLabel.Text = sliderRotation.Value.ToString("n2");
+                GetCameraPositionsInfo();
+            }
+
+            //forth quater
+            if ((e.NewValue >= 15) && (e.NewValue <= 20))
+            {
+                _urhoArea.CameraNode.Rotation = new Quaternion((float)(360 - MathHelper.RadiansToDegrees((Math.Acos((e.NewValue-15) / 5)))), 0, 0);
+                _urhoArea.CameraNode.Position = new Vector3(oldXPosition, -GetYCoordinate(20 - e.NewValue), (float)(20 - e.NewValue));
+
+                var xCircleRotation = _urhoArea.CameraNode.Rotation.X;
+                var xAngle = _urhoArea.CameraNode.Rotation.ToEulerAngles();
+                rotationValueLabel.Text = xAngle.X.ToString();
+
+                sliderValueLabel.Text = sliderRotation.Value.ToString("n2");
+                GetCameraPositionsInfo();
+            }
+        }
+
+        //  rhombus rotation
+        private void X_Rombus_Rotation_Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var oldXPosition = _urhoArea.CameraNode.Position.X;
+            var oldYPosition = _urhoArea.CameraNode.Position.Y;
+            var oldZPosition = _urhoArea.CameraNode.Position.Z;
+
+            if (e.NewValue < 5)
+            {
+                _urhoArea.CameraNode.Position = new Vector3(oldXPosition, (float)e.NewValue, (float)e.NewValue);
+                _urhoArea.CameraNode.Rotation = new Quaternion((float)(e.NewValue*18) , 0, 0);
+                var xCircleRotation = _urhoArea.CameraNode.Rotation.X;
+                rotationValueLabel.Text = xCircleRotation.ToString("n2");
+                sliderValueLabel.Text = sliderRotation.Value.ToString("n2");
+                GetCameraPositionsInfo();
+            }
+            if ((e.NewValue > 5) && (e.NewValue < 10))
+            {
+                _urhoArea.CameraNode.Position = new Vector3(oldXPosition, (float)(10 - e.NewValue), (float)e.NewValue);
+                _urhoArea.CameraNode.Rotation = new Quaternion((float)(e.NewValue*18), 0, 0);
+                var xCircleRotation = _urhoArea.CameraNode.Rotation.X;
+                rotationValueLabel.Text = xCircleRotation.ToString("n2");
+                sliderValueLabel.Text = sliderRotation.Value.ToString("n2");
+                GetCameraPositionsInfo();
+            }
+            if ((e.NewValue > 10) && (e.NewValue < 15))
+            {
+                _urhoArea.CameraNode.Position = new Vector3(oldXPosition, (float)(10 - e.NewValue), (float)(20 - e.NewValue));
+                _urhoArea.CameraNode.Rotation = new Quaternion((float)e.NewValue * 18, 0, 0);
+                var xCircleRotation = _urhoArea.CameraNode.Rotation.X;
+                rotationValueLabel.Text = xCircleRotation.ToString("n2");
+                sliderValueLabel.Text = sliderRotation.Value.ToString("n2");
+                GetCameraPositionsInfo();
+            }
+            if (e.NewValue > 15)
+            {
+                _urhoArea.CameraNode.Position = new Vector3(oldXPosition, (float)(e.NewValue - 20), (float)(20 - e.NewValue));
+                _urhoArea.CameraNode.Rotation = new Quaternion((float)e.NewValue * 18, 0, 0);
+                var yPos = _urhoArea.CameraNode.Position.Y;
+                var zPos = _urhoArea.CameraNode.Position.Z;
+                var xCircleRotation = _urhoArea.CameraNode.Rotation.X;
+                rotationValueLabel.Text = xCircleRotation.ToString("n2");
+                sliderValueLabel.Text = sliderRotation.Value.ToString("n2");
+                GetCameraPositionsInfo();
+            }
+        }
+
+        private void Y_Rotation_Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            {
+                var oldYPosition = _urhoArea.CameraNode.Position.Y;
+
+                //first quater
+                if (e.NewValue <= 5)
+                {
+                    _urhoArea.CameraNode.Rotation = new Quaternion(0, (float)MathHelper.RadiansToDegrees((Math.Acos((5 - e.NewValue) / 5))), 0);
+                    _urhoArea.CameraNode.Position = new Vector3(GetXCoordinate(e.NewValue), oldYPosition,  (float)e.NewValue);
+
+                    var xAngle = _urhoArea.CameraNode.Rotation.ToEulerAngles();
+                    rotationValueLabel.Text = xAngle.Y.ToString();
+
+                    sliderValueLabel.Text = sliderRotationY.Value.ToString("n2");
+                    GetCameraPositionsInfo();
+                }
+
+                //second quater
+                if ((e.NewValue >= 5) && (e.NewValue < 10))
+                {
+                    var asd = _urhoArea.CameraNode.Rotation.ToEulerAngles();
+
+                    _urhoArea.CameraNode.Position = new Vector3(GetXCoordinate(e.NewValue), oldYPosition,  (float)e.NewValue);
+                    _urhoArea.CameraNode.Rotation = new Quaternion( 0, (float)(180 - MathHelper.RadiansToDegrees(Math.Acos((e.NewValue - 5) / 5))), 0);
+
+                    var xAngle = _urhoArea.CameraNode.Rotation.ToEulerAngles();
+                    rotationValueLabel.Text = xAngle.Y.ToString();
+
+                    sliderValueLabel.Text = sliderRotationY.Value.ToString("n2");
+                    GetCameraPositionsInfo();
+                }
+
+                //third quater
+                if ((e.NewValue >= 10) && (e.NewValue < 15))
+                {
+                    _urhoArea.CameraNode.Rotation = new Quaternion( 0, (float)(270 - MathHelper.RadiansToDegrees((Math.Asin((15 - e.NewValue) / 5)))), 0);
+                    _urhoArea.CameraNode.Position = new Vector3(-GetXCoordinate(20 - e.NewValue),oldYPosition,  (float)(20 - e.NewValue));
+
+                    var xAngle = _urhoArea.CameraNode.Rotation.ToEulerAngles();
+                    rotationValueLabel.Text = xAngle.Y.ToString();
+
+                    sliderValueLabel.Text = sliderRotationY.Value.ToString("n2");
+                    GetCameraPositionsInfo();
+                }
+
+                //forth quater
+                if ((e.NewValue >= 15) && (e.NewValue <= 20))
+                {
+                    _urhoArea.CameraNode.Rotation = new Quaternion( 0, (float)(360 - MathHelper.RadiansToDegrees((Math.Acos((e.NewValue - 15) / 5)))), 0);
+                    _urhoArea.CameraNode.Position = new Vector3(-GetXCoordinate(20 - e.NewValue), oldYPosition,  (float)(20 - e.NewValue));
+
+                    var xAngle = _urhoArea.CameraNode.Rotation.ToEulerAngles();
+                    rotationValueLabel.Text = xAngle.Y.ToString();
+
+                    sliderValueLabel.Text = sliderRotationY.Value.ToString("n2");
+                    GetCameraPositionsInfo();
+                }
+            }
+        }
+
+        private void SetCameraPanParameters()
+        {
+            _panCameraPositionX = _urhoArea.CameraNode.Position.X;
+            _panCameraPositionY = _urhoArea.CameraNode.Position.Y;
+            _panCameraPositionZ = _urhoArea.CameraNode.Position.Z;
+            _panCameraRotationX = _urhoArea.CameraNode.Rotation.X;
+        }
+
+        private void Restart_Button_Clicked(object sender, EventArgs e)
+        {
+            _urhoArea.CameraNode.Position = new Vector3(0, 0, 0);
+            _urhoArea.CameraNode.Rotation = new Quaternion(0, 0, 0);
+            rotationValueLabel.Text = "0";
+            SetCameraPanParameters();
+            GetCameraPositionsInfo();
+        }
+
+        private float GetYCoordinate(double z)
+        {
+            var zCoord = (float)Math.Sqrt(z * (10-z));
+            return zCoord;
+        }
+
+        private float GetXCoordinate(double z)
+        {
+            var xCoord = (float)Math.Sqrt(z * (10 - z));
+            return - xCoord;
+        }
+
+        private void GetCameraPositionsInfo()
+        {
+            x_pos_label.Text = _urhoArea.CameraNode.Position.X.ToString("n2");
+            y_pos_label.Text = _urhoArea.CameraNode.Position.Y.ToString("n2");
+            z_pos_label.Text = _urhoArea.CameraNode.Position.Z.ToString("n2");
+        }
+
+        private void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ UrhoTestApp/UrhoTestApp/SceneHandler.cs
@@ -0,0 +1,135 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using Urho;
+using Urho.Shapes;
+
+namespace UrhoTestApp
+{
+    public class SceneHandler: Application, INotifyPropertyChanged
+    {
+        public Node CameraNode { get; set; }
+        private Color _legColor;
+        private Color _plainColor;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public SceneHandler(ApplicationOptions options) : base(options)
+        {
+        }
+
+        protected override async void Start()
+        {
+            base.Start();
+            _legColor = Color.FromHex("#cc5200");
+            _plainColor = Color.FromHex("#cc5200");
+            await  CreateTableModel();
+        }
+
+        private async Task CreateTableModel()
+        {
+            var scene = new Scene();
+            var octree = scene.CreateComponent<Octree>();
+
+            var zoneNode = scene.CreateChild("Zone");
+            var zone = zoneNode.CreateComponent<Zone>();
+
+            // Set same volume as the Octree, set a close bluish fog and some ambient light
+            zone.SetBoundingBox(new BoundingBox(-1000.0f, 1000.0f));
+            zone.AmbientColor = Color.FromHex("#131E4B");
+            zone.FogColor = Color.FromHex("#131E4B");
+            zone.FogStart = 100;
+            zone.FogEnd = 300;
+
+
+            //Table creation
+            //first leg
+            Node nodeleg1 = scene.CreateChild();
+            nodeleg1.Position = new Vector3(-0.5f, 0, 4.5f);
+            nodeleg1.Rotation = new Quaternion(0, 0, 0);
+            nodeleg1.SetScaleSilent(new Vector3(0.3f, 1f,0.3f));
+
+            var modelObjectLeg1 = nodeleg1.CreateComponent<Urho.Shapes.Cylinder>();
+            modelObjectLeg1.Color = _legColor;
+
+
+            //second leg
+            Node nodeleg2 = scene.CreateChild();
+            nodeleg2.Position = new Vector3(0.5f, 0, 4.5f);
+            nodeleg2.Rotation = new Quaternion(0, 0, 0);
+            nodeleg2.SetScaleSilent(new Vector3(0.3f, 1f, 0.3f));
+
+            var modelObjectLeg2 = nodeleg2.CreateComponent<Urho.Shapes.Cylinder>();
+            modelObjectLeg2.Color = _legColor;
+
+            //third leg
+            Node nodeleg3 = scene.CreateChild();
+            nodeleg3.Position = new Vector3(-0.5f, 0, 5.5f);
+            nodeleg3.Rotation = new Quaternion(0, 0, 0);
+            nodeleg3.SetScaleSilent(new Vector3(0.3f, 1f, 0.3f));
+
+            var modelObjectLeg3 = nodeleg3.CreateComponent<Urho.Shapes.Cylinder>();
+            modelObjectLeg3.Color = _legColor;
+
+
+            //forth leg
+            Node nodeleg4 = scene.CreateChild();
+            nodeleg4.Position = new Vector3(0.5f, 0, 5.5f);
+            nodeleg4.Rotation = new Quaternion(0, 0, 0);
+            nodeleg4.SetScaleSilent(new Vector3(0.3f, 1f, 0.3f));
+
+            var modelObjectLeg4 = nodeleg4.CreateComponent<Urho.Shapes.Cylinder>();
+            modelObjectLeg4.Color = _legColor;
+
+            //box
+
+            Node boxNode = scene.CreateChild();
+            boxNode.Position = new Vector3(0, 0.5f, 5);
+            boxNode.Rotation = new Quaternion(0, 0, 0);
+            boxNode.SetScaleSilent(new Vector3(1.8f, 0.1f, 1.8f));
+            var box = boxNode.CreateComponent<Box>();
+            box.Color = _plainColor;
+
+            //axes
+            Node axeXNode = scene.CreateChild();
+            axeXNode.Position = new Vector3(0, 0, 5);
+            axeXNode.SetScaleSilent(new Vector3(50, 0.05f, 0.05f));
+            var axeX = axeXNode.CreateComponent<Cylinder>();
+            axeX.Color = Color.Yellow;
+
+            Node axeYNode = scene.CreateChild();
+            axeYNode.Position = new Vector3(0, 0, 5);
+            axeYNode.SetScaleSilent(new Vector3(0.05f, 50, 0.05f));
+            var axeY = axeYNode.CreateComponent<Cylinder>();
+            axeY.Color = Color.Cyan;
+
+            Node axeZNode = scene.CreateChild();
+            axeZNode.Position = new Vector3(0, 0, 5);
+            axeZNode.SetScaleSilent(new Vector3(0.05f, 0.05f, 50));
+            var axeZ = axeZNode.CreateComponent<Cylinder>();
+            axeZ.Color = Color.White;
+
+
+            //  light & camera
+            Node light = scene.CreateChild(name: "light");
+            light.SetDirection(new Vector3(0.4f, -0.5f, 0.3f));
+            light.CreateComponent<Light>();
+            light.Position = new Vector3(0, 3, 0);
+
+            Node light2 = scene.CreateChild(name: "light");
+            light2.SetDirection(new Vector3(-0.4f, 0.5f, 0.3f));
+            light2.CreateComponent<Light>();
+            light2.Position = new Vector3(5, 0, 5);
+
+            Node light3 = scene.CreateChild(name: "light");
+            light3.SetDirection(new Vector3(0,0,0));
+            light3.CreateComponent<Light>();
+            light3.Position = new Vector3(2, 2, 3);
+
+            CameraNode = scene.CreateChild(name: "camera");
+            Camera camera = CameraNode.CreateComponent<Camera>();
+            Renderer.SetViewport(0, new Viewport(scene, camera, null));
+            CameraNode.Rotation = new Quaternion(0, 0, 0);
+            CameraNode.Position = new Vector3(0, 0, 0);
+        }
+    }
+}
